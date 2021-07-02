7d8e9be8babcc3a8727c3be16beea57848830f2a
Dale <dale@internold.com>
2021-07-02 10:15:09 +0800

    Changed wp-config.php database credentials

--- wp-config.php
+++ wp-config.php
@@ -19,17 +19,18 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
 
-/** MySQL database username */
-define( 'DB_USER', 'root' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+if(strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+	define( 'DB_NAME', 'local' );
+	define( 'DB_USER', 'root' );
+	define( 'DB_PASSWORD', 'root' );
+	define( 'DB_HOST', 'localhost' );
+} else {
+	define( 'DB_NAME', 'dbsataoapeui04' );
+	define( 'DB_USER', 'u5tw5mgmokhvu' );
+	define( 'DB_PASSWORD', 'h2J}1#4k@36F' );
+	define( 'DB_HOST', '127.0.0.1' );
+}
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
