0223e50a0aae1941ab2a8dbde90e21c4762d4baf
Dale <dale@internold.com>
2021-09-06 11:53:17 +0800

    Removed footer credits

--- wp-content/themes/eksell/footer.php
+++ wp-content/themes/eksell/footer.php
@@ -23,13 +23,7 @@
 
 					<div class="footer-credits">
 
-						<p class="footer-copyright">&copy; <?php echo esc_html( date_i18n( esc_html__( 'Y', 'eksell' ) ) ); ?> <a href="<?php echo esc_url( home_url() ); ?>" rel="home"><?php echo bloginfo( 'name' ); ?></a></p>
-
-						<p class="theme-credits color-secondary">
-							<?php
-							// Translators: $s = name of the theme developer.
-							printf( esc_html_x( 'Theme by %s', 'Translators: $s = name of the theme developer', 'eksell' ), '<a href="https://www.andersnoren.se">' . esc_html__( 'Anders Nor&eacute;n', 'eksell' ) . '</a>' ); ?>
-						</p><!-- .theme-credits -->
+						<p class="footer-copyright">&copy; <?php echo esc_html( date_i18n( esc_html__( 'Y', 'eksell' ) ) ); ?> </p>
 
 					</div><!-- .footer-credits -->
 
